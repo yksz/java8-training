dd24338b8c75ddd3cde20d6142661a032665aa96
yksz <user4git@gmail.com>
2014-08-18 02:01:16 +0900

    Add ex01_02

---
+++ src/ch01/ex01_02/Main.java
@@ -0,0 +1,17 @@
+package ch01.ex01_02;
+
+import java.io.File;
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        File targetDir = new File(".");
+        if (args.length > 0)
+            targetDir = new File(args[0]);
+
+        File[] dirs = targetDir.listFiles(file -> file.isDirectory());
+        Arrays.stream(dirs).forEach(f -> System.out.println(f));
+    }
+
+}
