4e8a1fca02cc66bf1bc9cf66567905d8207e83a6
yksz <user4git@gmail.com>
2014-08-20 02:41:33 +0900

    Add ex01_04

---
+++ src/ch01/ex01_04/Main.java
@@ -0,0 +1,25 @@
+package ch01.ex01_04;
+
+import java.io.File;
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        File targetDir = new File(".");
+        if (args.length > 0)
+            targetDir = new File(args[0]);
+
+        File[] files = targetDir.listFiles();
+        Arrays.sort(files, (first, second) -> {
+           if (first.isDirectory() && second.isFile())
+               return -1;
+           else if (first.isFile() && second.isDirectory())
+               return 1;
+           else
+               return 0;
+        });
+        Arrays.stream(files).forEach(f -> System.out.println(f));
+    }
+
+}
