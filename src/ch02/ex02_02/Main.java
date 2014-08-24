55fa0d97f8203d0ed8f9bf71a0a12b0743d77feb
yksz <user4git@gmail.com>
2014-08-25 00:54:18 +0900

    Add ex02_02

---
+++ src/ch02/ex02_02/Main.java
@@ -0,0 +1,17 @@
+package ch02.ex02_02;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String contents = "abcdefg abcdef abcde abcd abc ab a";
+        List<String> words = Arrays.asList(contents.split(" "));
+        words.stream().filter(w -> {
+            System.out.println(w);
+            return w.length() >= 3;
+        }).limit(5).count();
+    }
+
+}
