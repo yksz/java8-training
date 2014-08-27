d6803cf9794ee7b1f288f5fd286b26ef880c4e55
yksz <user4git@gmail.com>
2014-08-27 18:10:45 +0900

    Add ex02_05

---
+++ src/main/java/ch02/ex02_05/Main.java
@@ -0,0 +1,18 @@
+package ch02.ex02_05;
+
+import java.util.stream.Stream;
+
+public class Main {
+
+    static Stream<Long> newRandomStream(long seed) {
+        long a = 25214903917L;
+        int c = 11;
+        long m = (long) Math.pow(2, 48);
+        return Stream.iterate(seed, x -> (a*x + c) % m);
+    }
+
+    public static void main(String[] args) {
+        newRandomStream(0).limit(5).forEach(System.out::println);
+    }
+
+}
