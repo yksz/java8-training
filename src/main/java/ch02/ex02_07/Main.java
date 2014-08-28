3643135edab68e636bd6b2edac9c6529ae199cad
yksz <user4git@gmail.com>
2014-08-28 21:41:47 +0900

    Add ex02_07

---
+++ src/main/java/ch02/ex02_07/Main.java
@@ -0,0 +1,21 @@
+package ch02.ex02_07;
+
+import java.util.Spliterator;
+import java.util.stream.Stream;
+
+public class Main {
+
+    // isFinite は終端操作であるため実用的ではない
+    static <T> boolean isFinite(Stream<T> stream) {
+        Spliterator<T> spliterator = stream.spliterator();
+        return spliterator.hasCharacteristics(Spliterator.SIZED);
+    }
+
+    public static void main(String[] args) {
+        Stream<Integer> s1 = Stream.of(1, 2, 3);
+        Stream<Integer> s2 = Stream.iterate(0, n -> n + 1);
+        System.out.println(isFinite(s1));
+        System.out.println(isFinite(s2));
+    }
+
+}
