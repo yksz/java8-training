398a0e27a0716e65d022f9a6438cab4fabe54a42
yksz <user4git@gmail.com>
2014-08-29 20:32:28 +0900

    Add ex02_10

---
+++ src/main/java/ch02/ex02_10/Main.java
@@ -0,0 +1,26 @@
+package ch02.ex02_10;
+
+import java.util.stream.DoubleStream;
+import java.util.stream.Stream;
+
+public class Main {
+
+    static double average(Stream<Double> stream) {
+        double[] result = stream
+                .map(n -> new double[]{ n, 0.0 })
+                .reduce(new double[2], (acc, val) -> {
+                    acc[0] += val[0];
+                    acc[1] += 1.0;
+                    return acc;
+                });
+        double sum = result[0];
+        double count = result[1];
+        return sum / count;
+    }
+
+    public static void main(String[] args) {
+        double[] values = { 1.0, 2.0, 3.0, 4.0, 5.0 };
+        System.out.println(average(DoubleStream.of(values).boxed()));
+    }
+
+}
