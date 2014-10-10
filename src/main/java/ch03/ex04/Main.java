e4aed0ca56d2ad31c77450a31c6520d2205f2e35
yksz <user4git@gmail.com>
2014-10-11 01:36:50 +0900

    Add ch03/ex04

---
+++ src/main/java/ch03/ex04/Main.java
@@ -0,0 +1,14 @@
+package ch03.ex04;
+
+public class Main {
+
+    // java.nio.file.DirectoryStream.Filter
+    // javax.xml.stream.EventFilter
+    // java.io.FileFilter
+    // java.io.FilenameFilter => 複数の引数を受け入れるため付加価値がある
+    // java.util.logging.Filter
+    // javax.management.NotificationFilter
+    // javax.imageio.spi.ServiceRegistry.Filter
+    // javax.xml.stream.StreamFilter
+
+}
