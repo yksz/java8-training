8eee982bad32b99921a414dffbdb79160618fbfe
yksz <user4git@gmail.com>
2014-10-14 15:43:49 +0900

    Remove 'synchronized' from ch03/ex03

--- src/main/java/ch03/ex03/Assertion.java
+++ src/main/java/ch03/ex03/Assertion.java
@@ -2,11 +2,12 @@ package ch03.ex03;
 
 import java.util.function.Supplier;
 
+// Not thread safe
 public class Assertion {
 
     public static boolean isEnabled = false;
 
-    public static synchronized void setEnabled(boolean b) {
+    public static void setEnabled(boolean b) {
         isEnabled = b;
     }
 
