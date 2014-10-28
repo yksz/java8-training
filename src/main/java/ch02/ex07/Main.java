e4943814b9c3dec48c84ebf432b98ec3194ba3a2
yksz <user4git@gmail.com>
2014-10-28 23:31:04 +0900

    Add a test case in ch02/ex07

--- src/main/java/ch02/ex07/Main.java
+++ src/main/java/ch02/ex07/Main.java
@@ -14,8 +14,10 @@ public class Main {
     public static void main(String[] args) {
         Stream<Integer> s1 = Stream.of(1, 2, 3);
         Stream<Integer> s2 = Stream.iterate(0, n -> n + 1);
+        Stream<Integer> s3 = Stream.iterate(0, n -> n + 1).limit(10);
         assert isFinite(s1) == true;
         assert isFinite(s2) == false;
+        assert isFinite(s3) == false;
     }
 
 }
