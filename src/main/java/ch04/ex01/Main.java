b1f9dbd3b86b87fc7f30fbb0623236063b9f5e70
yksz <user4git@gmail.com>
2014-11-18 14:43:21 +0900

    Add ch04/ex01

---
+++ src/main/java/ch04/ex01/Main.java
@@ -0,0 +1,30 @@
+package ch04.ex01;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    public void start(Stage stage) {
+        String message = "Hello, FX";
+        Label label = new Label("Hello, FX");
+        label.setFont(new Font(100));
+        TextField textField = new TextField(message);
+        textField.setOnAction(event -> label.setText(textField.getText()));
+        VBox root = new VBox();
+        root.getChildren().addAll(textField, label);
+        stage.setScene(new Scene(root));
+        stage.setTitle("Hello");
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+}
