97b68a70c0e87f1ceef75667cc9f6980d4f683bb
yksz <user4git@gmail.com>
2014-12-09 23:48:54 +0900

    Add ch05/ex02

---
+++ src/main/java/ch05/ex02/Main.java
@@ -0,0 +1,17 @@
+package ch05.ex02;
+
+import java.time.LocalDate;
+
+public class Main {
+
+    public static void main(String[] args) {
+        LocalDate date = LocalDate.of(2000, 2, 29);
+        assert date.plusYears(1).equals(LocalDate.of(2001, 2, 28));
+        assert date.plusYears(4).equals(LocalDate.of(2004, 2, 29));
+        LocalDate result = date;
+        for (int i = 0; i < 4; i++)
+            result = result.plusYears(1);
+        assert result.equals(LocalDate.of(2004, 2, 28));
+    }
+
+}
