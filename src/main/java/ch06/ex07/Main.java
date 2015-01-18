37f5c500c66d79292a59b6847785f3b66fd62b18
yksz <user4git@gmail.com>
2015-01-18 23:49:20 +0900

    Add ch06/ex07

---
+++ src/main/java/ch06/ex07/Main.java
@@ -0,0 +1,16 @@
+package ch06.ex07;
+
+import java.util.Map.Entry;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.stream.LongStream;
+
+public class Main {
+
+    public static void main(String[] args) {
+        ConcurrentHashMap<String, Long> map = new ConcurrentHashMap<>();
+        LongStream.range(0, 1000).parallel().forEach(l -> map.put(String.valueOf(l), l));
+        Entry<String, Long> e = map.reduceEntries(1, (e1, e2) -> e1.getValue() > e2.getValue() ? e1 : e2);
+        assert e.getKey().equals("999");
+    }
+
+}
