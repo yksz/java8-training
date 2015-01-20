6746bedc9d6aa4db45a8039904a84d98f7d49ff4
yksz <user4git@gmail.com>
2015-01-21 01:37:20 +0900

    Add ch06/ex05

---
+++ src/main/java/ch06/ex05/Reader.java
@@ -0,0 +1,66 @@
+package ch06.ex05;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
+
+public class Reader {
+
+    private ConcurrentMap<String, Set<File>> map = new ConcurrentHashMap<>();
+
+    public void read(File... files) throws InterruptedException {
+        List<Thread> threads = new ArrayList<>();
+        for (File file : files) {
+            Thread th = new Thread(() -> put(getWords(file), file));
+            th.start();
+            threads.add(th);
+        }
+        for (Thread th : threads) {
+            th.join();
+        }
+    }
+
+    private String[] getWords(File file) {
+        try {
+            String contents = new String(Files.readAllBytes(file.toPath()), StandardCharsets.UTF_8);
+            return contents.split("[\\P{L}]+");
+        } catch (IOException e) {
+            e.printStackTrace();
+            return new String[0];
+        }
+    }
+
+    private void put(String[] words, File file) {
+        for (String word : words) {
+            Set<File> set = new HashSet<>();
+            set.add(file);
+            map.merge(word, set, (existingValue, newValue) -> {
+                existingValue.addAll(newValue);
+                return existingValue;
+            });
+        }
+    }
+
+    static URL getResource(String resource) {
+        ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
+        return classLoader.getResource(resource);
+    }
+
+    public static void main(String[] args) throws Exception {
+        File dir = new File(getResource("ch06").toURI());
+        Reader reader = new Reader();
+        reader.read(dir.listFiles(file -> file.getName().matches(".+txt$")));
+        reader.map.forEach((key, value) -> {
+            System.out.println(key + ": " + value);
+        });
+    }
+
+}
