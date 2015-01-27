8402001612e87a0b6a2aee0629cac2a0d58638f4
yksz <user4git@gmail.com>
2015-01-27 22:12:55 +0900

    Add ch07/ex07

---
+++ src/main/javascript/ch07/ex07/pipe.js
@@ -0,0 +1,39 @@
+var byteArray = Java.type('byte[]');
+
+function pipe(commands) {
+  var pb = new java.lang.ProcessBuilder();
+  var ps = commands.map(function(e, i, a) {
+    return pb.command(e.split(/ /)).start();
+  });
+  var p = pipeTo(ps);
+  plug(p.getInputStream(), java.lang.System.out);
+  p.waitFor();
+}
+
+function pipeTo(processes) {
+  for (var i = 0; i < processes.length - 1; i++) {
+    plug(processes[i].getInputStream(), processes[i + 1].getOutputStream());
+  }
+  return processes.pop();
+}
+
+function plug(inputStream, outputStream) {
+  new java.lang.Thread(function() {
+    var input = new java.io.BufferedInputStream(inputStream);
+    var output = new java.io.BufferedOutputStream(outputStream);
+    var len;
+    var buf = new byteArray(1024);
+    try {
+      while ((len = input.read(buf)) != -1)
+        output.write(buf, 0, len);
+    } catch (e) {
+      e.printStackTrace();
+    } finally {
+      output.close();
+    }
+  }).start();
+}
+
+
+var commands = ['cat pipe.js', 'sort'];
+pipe(commands);
