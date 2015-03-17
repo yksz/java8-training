920368a2a15129956701b48cabe64f96ea3bedc4
yksz <user4git@gmail.com>
2015-03-17 23:36:23 +0900

    Add ch09/ex06

---
+++ src/main/java/ch09/ex06/Main.java
@@ -0,0 +1,27 @@
+package ch09.ex06;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Iterator;
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        List<String> lines = Files.readAllLines(Paths.get("/usr/share/dict/words"));
+        Files.write(Paths.get("/tmp/out.txt"), new Iterable<String>() {
+            public Iterator<String> iterator() {
+                return new Iterator<String>() {
+                    public boolean hasNext() {
+                        return !lines.isEmpty();
+                    }
+                    public String next() {
+                        return lines.remove(lines.size() - 1);
+                    }
+                };
+            }
+        });
+    }
+
+}
