21d0d796dee49fa29bc084f3717cb54e5e014116
yksz <user4git@gmail.com>
2015-04-10 11:52:39 +0900

    Correct ch07/ex03

--- src/main/javascript/ch07/ex03/biginteger.js
+++ src/main/javascript/ch07/ex03/biginteger.js
@@ -2,3 +2,5 @@ var b = new java.math.BigInteger('1234567890987654321');
 print(b); // => 1234567890987654400
 var m = b.mod(java.math.BigInteger.TEN);
 print(m); // => 1
+
+print(java.lang.String.format('%d', b)); // => 1234567890987654321
